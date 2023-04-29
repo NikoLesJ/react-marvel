f33040f08299983dcdc8be9a562095d7bd6a430e
Niko Les <3gdoma'gmail.com>
2023-04-29 13:42:24 +0200

    Test project

---
+++ src/services/MarvelService.js
@@ -0,0 +1,40 @@
+
+
+class MarvelService {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=23d97fdead1b5cd7da6730809da4c1b3';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Cloud not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=211&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
+    }
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'Information about this character is shrouded in mystery...',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            comics: char.comics.items
+        }
+    }
+}
+
+export default MarvelService;
